b5b1754ceeebdba01a32fd4514a6d44f73adec4b
Alexandra Debartolo <34449443+alexandradebartolo@users.noreply.github.com>
2019-08-22 00:01:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// prints "hi" in the browser's dev tools console
 console.log('hi');
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
 console.log('hi');
- $(document).ready(function(){
-    $('.modal').modal();
-  });
+$(document).ready(function(){
+  $('.modal').modal();
+});
